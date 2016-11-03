b0c8f119e3a6196807adb2e680e58ec852d16b5b
xuyouhong <1647064983@qq.com>
2016-11-03 18:05:18 +0800

    create weex project

---
+++ src/util.js
@@ -0,0 +1,65 @@
+
+//这个文件是用于放一些全局的变量， 便于修改
+var apiBase = 'https://dev-api2.toomao.com';
+//初次对JS中的exports关键字的使用
+exports.serverPath = function() {
+	// console.log(apiBase);
+	return apiBase;
+}
+
+//所有价格的保留位数
+var toFixedNum = 2;
+exports.fixedNum = function () {
+	// body...
+	return toFixedNum;
+}
+
+//统一设备宽度
+var deviceWidth = 750;
+exports.deviceWidth = function (thisObj) {
+	// 获取设备宽度
+	// var deviceWidth = thisObj.$getConfig().env.deviceWidth;
+	return 750;
+}
+
+
+//获取navigator URL
+var bundleUrl = '';
+exports.bundleUrl = function (thisObj) {
+
+			var dir = 'dist';
+			var bundleUrl = thisObj.$getConfig().bundleUrl;
+            bundleUrl = new String(bundleUrl);
+
+            var nativeBase;
+            var isAndroidAssets = bundleUrl.indexOf('file://assets/') >= 0;
+
+            var isiOSAssets = bundleUrl.indexOf('file:///') >= 0 && bundleUrl.indexOf('WeexDemo.app') > 0;
+            if (isAndroidAssets) {
+              nativeBase = 'file://assets/';
+            }
+            else if (isiOSAssets) {
+              nativeBase = bundleUrl.substring(0, bundleUrl.lastIndexOf('/') + 1);
+            }
+            else {
+             //http://127.0.0.1:12580/examples/build/reading.js
+              var host = 'localhost:12580';
+              var matches = /\/\/([^\/]+?)\//.exec(thisObj.$getConfig().bundleUrl);
+              if (matches && matches.length >= 2) {
+                host = matches[1];
+              }
+              //nativeBase = 'http://' + host + '/weex_tmp/h5_render/';
+              nativeBase = 'http://' + host + '/' + dir + '/';// + '/build/';
+            }
+            // 暂时没有理解这边路径的设置
+            var h5Base = './index.html?page=./' + dir + '/';// + '/build/';
+            //Native端
+            var base = nativeBase;
+            //H5端  
+            if (typeof window === 'object') {
+              base = h5Base;
+            }
+            return base;
+}
+
+
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+require('webpack');
+require('weex-loader');
+
+var path = require('path');
+var fs=require('fs')
+
+var entry={};
+function walk(dir, root) {
+  var directory = path.join(__dirname, root, dir)
+  fs.readdirSync(directory)
+      .forEach(function (file) {
+        var fullpath = path.join(directory, file)
+        var stat = fs.statSync(fullpath)
+
+        if (stat.isFile() &&
+            path.extname(fullpath) === '.we') {
+          var name = path.join( dir, path.basename(file, '.we'))
+          entry[name] = fullpath + '?entry=true'
+        } else if (stat.isDirectory()) {
+          var subdir = path.join(dir, file)
+          walk(subdir, root)
+        }
+      })
+}
+walk('./', 'src')
+module.exports = {
+  entry: entry,
+  output: {
+    path: 'dist',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.we(\?[^?]+)?$/,
+        loaders: ['weex-loader']
+      }
+    ]
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// require('webpack')
+// require('weex-loader')
+
+// var path = require('path')
+
+// module.exports = {
+//   entry: {
+//     main: path.join(__dirname, 'src', 'main.we?entry=true')
+//   },
+//   output: {
+//     path: 'dist',
+//     filename: '[name].js'
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.we(\?[^?]+)?$/,
+//         loaders: ['weex-loader']
+//       }
+//     ]
+//   }
+// }
