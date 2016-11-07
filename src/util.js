269bbc111cb22206890ea8c6fb63e2c4139e75b6
xuyouhong <1647064983@qq.com>
2016-11-07 17:25:54 +0800

    add mine page

--- src/util.js
+++ src/util.js
@@ -31,35 +31,35 @@ exports.bundleUrl = function (thisObj) {
 			var bundleUrl = thisObj.$getConfig().bundleUrl;
             bundleUrl = new String(bundleUrl);
 
-            var nativeBase;
-            var isAndroidAssets = bundleUrl.indexOf('file://assets/') >= 0;
+      var nativeBase;
+      var isAndroidAssets = bundleUrl.indexOf('file://assets/') >= 0;
 
-            var isiOSAssets = bundleUrl.indexOf('file:///') >= 0 && bundleUrl.indexOf('WeexDemo.app') > 0;
-            if (isAndroidAssets) {
-              nativeBase = 'file://assets/';
-            }
-            else if (isiOSAssets) {
-              nativeBase = bundleUrl.substring(0, bundleUrl.lastIndexOf('/') + 1);
-            }
-            else {
-             //http://127.0.0.1:12580/examples/build/reading.js
-              var host = 'localhost:12580';
-              var matches = /\/\/([^\/]+?)\//.exec(thisObj.$getConfig().bundleUrl);
-              if (matches && matches.length >= 2) {
-                host = matches[1];
-              }
-              //nativeBase = 'http://' + host + '/weex_tmp/h5_render/';
-              nativeBase = 'http://' + host + '/' + dir + '/';// + '/build/';
-            }
-            // 暂时没有理解这边路径的设置
-            var h5Base = './index.html?page=./' + dir + '/';// + '/build/';
-            //Native端
-            var base = nativeBase;
-            //H5端  
-            if (typeof window === 'object') {
-              base = h5Base;
-            }
-            return base;
+      var isiOSAssets = bundleUrl.indexOf('file:///') >= 0 && bundleUrl.indexOf('WeexDemo.app') > 0;
+      if (isAndroidAssets) {
+        nativeBase = 'file://assets/';
+      }
+      else if (isiOSAssets) {
+        nativeBase = bundleUrl.substring(0, bundleUrl.lastIndexOf('/') + 1);
+      }
+      else {
+       //http://127.0.0.1:12580/examples/build/reading.js
+        var host = 'localhost:12580';
+        var matches = /\/\/([^\/]+?)\//.exec(thisObj.$getConfig().bundleUrl);
+        if (matches && matches.length >= 2) {
+          host = matches[1];
+        }
+        //nativeBase = 'http://' + host + '/weex_tmp/h5_render/';
+        nativeBase = 'http://' + host + '/' + dir + '/';// + '/build/';
+      }
+      // 暂时没有理解这边路径的设置
+      var h5Base = './index.html?page=./' + dir + '/';// + '/build/';
+      //Native端
+      var base = nativeBase;
+      //H5端  
+      if (typeof window === 'object') {
+        base = h5Base;
+      }
+      return base;
 }
 
 
